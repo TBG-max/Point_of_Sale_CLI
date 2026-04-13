d2e2efea52a1b87d4963e21945a4cf842b41f67c
MathTeacherSmith <Clarence.Smith@vertexacademies.org>
2026-04-13 16:42:56 +0000

    setup files

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# main.py should start the app and work without any modifications
+
+# DO NOT CHANGE THIS CODE
+from module import PointOfSale
+app = PointOfSale()
+app.start()
---
+++ module.py
@@ -0,0 +1,10 @@
+# You will need to add code in this module to make teh Point of Sale Application functional.
+
+class PointOfSale:
+  def __init__(self):
+    self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
+    print("\nInitializing POS system...")
+    
+    
+  def start(self): # This is the function that should be used to start the application.
+    print("\nThe app is not complete.")
