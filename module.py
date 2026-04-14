7a8aaa56afeb90fd532a7f925660a7ea97abb684
TBG-max <cristian.perez@stu.vertexacademies.org>
2026-04-14 18:16:46 +0000

    added code

--- module.py
+++ module.py
@@ -5,11 +5,29 @@
 # You will need to add code in this module to make teh Point of Sale Application functional.
 
 
+from tkinter import Menu
+
+
 class PointOfSale:
   def __init__(self):
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
     print("\nInitializing POS system...")
+
     
     
   def start(self): # This is the function that should be used to start the application.
-    print("\nThe app is not complete.")
+    print("welcome to Cristian's POS.")
+    while True:
+      print(Menu)
+      choice = input("Enter your choice: ")
+      if choice == "1":
+        
+
+        
+      
+
+    
+    
+    
+    
+    '''print("\nThe app is not complete.")'''
